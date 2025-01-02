1f5d0be4830c70c1a0269c978568f8a74cf215fe
WHY-DOUGH <153290070+WHY-DOUGH@users.noreply.github.com>
2025-01-02 09:25:45 +0800

    Put fetch test

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // Main file of js
+
+fetch('/data/player.json')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.name); // Access data here
+    })
+    .catch(error => console.error('Error:', error));
